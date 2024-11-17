64664dbcd2b92117370cb192b2f324bde42fb886
Sumit Yadav <sumit2911997@gmail.com>
2024-11-17 12:47:15 +0530

    solve leetcode 236

---
+++ src/main/java/org/example/LCode236.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class LCode236 {
+
+    private static class TreeNode {
+        int val;
+        org.example.TreeNode left;
+        org.example.TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left != null) {
+            return left;
+        }
+
+        return right;
+    }
+
+}
